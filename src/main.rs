91175850e1cc75d9ab36478bb085065dcef8bb6a
alexander wickman <alexander.k.wickman@gmail.com>
2025-05-16 20:35:36 +0100

    add command

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,13 @@ fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
-        
+
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-        match input {
+        match input.trim() {
+            "exit 0" => {
+                break;
+            }
             _ => {
                 println!("{}: command not found", input.trim())
             }
